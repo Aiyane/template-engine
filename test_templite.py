04777dd296938e784d8be8fb9d7d1e761443a52a
Aiyane <2310091880@qq.com>
2018-01-14 21:37:50 +0800

    一点错误

--- test_templite.py
+++ test_templite.py
@@ -6,13 +6,14 @@ import re
 from templite import templite, TempliteSyntaxError
 from unittest import TestCase
 
+
 class AnyOldClass(object):
     def __init__(self, **attrs):
         for n, v in attrs.items():
             setattr(self, n, v)
 
-class TempliteTest(TestCase):
 
+class TempliteTest(TestCase):
     """测试类"""
 
     def __init__(self, text, ctx=None, result=None):
@@ -27,19 +28,17 @@ class TempliteTest(TestCase):
         if result:
             self.assertEqual(actual, result)
 
-    def asseretSynErr(self, msg):
+    def assertSynErr(self, msg):
         pat = "^" + re.escape(msg) + "$"
         return self.assertRaisesRegexp(TempliteSyntaxError, pat)
 
     def test_passthrough(self):
         self.assertEqual(Templite("Hello").render, "Hello")
         self.assertEqual(
-            Templite("Hello, 20% fun time!").render(),
-            "Hello, 20% fun time!"
-        )
+            Templite("Hello, 20% fun time!").render(), "Hello, 20% fun time!")
 
     def test_variables(self):
-        self.try_render("Hello, {{name}}!", {'name':'Ned'}, "Hello, Ned!")
+        self.try_render("Hello, {{name}}!", {'name': 'Ned'}, "Hello, Ned!")
 
     def test_undefined_variables(self):
         with self.assertRaises(Exception):
@@ -60,8 +59,8 @@ class TempliteTest(TestCase):
         }
 
         template = Templite("This is {{name|upper}}{{punct}}", globs)
-        self.assertEqual(template.render({'name':'Ned'}), "This is NED!")
-        self.assertEqual(template.render({'name':'Ben'}), "This is BEN!")
+        self.assertEqual(template.render({'name': 'Ned'}), "This is NED!")
+        self.assertEqual(template.render({'name': 'Ben'}), "This is BEN!")
 
     def test_attribute(self):
         """
@@ -79,58 +78,46 @@ class TempliteTest(TestCase):
         :returns: TODO
 
         """
+
         class WithMemberFns(AnyOldObject):
             """
             """
+
             def ditto(self):
                 """
                 """
                 return self.txt + self.txt
+
         obj = WithMemberFns(txt="Once")
         self.try_render("{{obj.ditto}}", locals(), "OnceOnce")
 
     def test_item_access(self):
-        d = {'a':17, 'b':23}
+        d = {'a': 17, 'b': 23}
         self.try_render("{{d.a}} < {{d.b}}", locals(), "17 < 23")
 
     def test_loops(self):
         nums = [1, 2, 3, 4]
-        self.try_render(
-            "Look:{% for n in num %}{{n}}, {% endfor %}done.",
-            locals(),
-            "Look: 1, 2, 3, 4, done."
-        )
-        def rev(1):
+        self.try_render("Look:{% for n in num %}{{n}}, {% endfor %}done.",
+                        locals(), "Look: 1, 2, 3, 4, done.")
+
+        def rev(l):
             l = l[:]
             l.reverse()
             return l
 
-        self.try_render(
-            "Look: {% for n in num|rev %}{{n}}, {% endfor %}done.",
-            locals(),
-            "Look:4, 3, 2, 1, done."
-        )
+        self.try_render("Look: {% for n in num|rev %}{{n}}, {% endfor %}done.",
+                        locals(), "Look:4, 3, 2, 1, done.")
 
     def test_empty_loops(self):
-        self.try_render(
-            "Empty: {% for n in num %}{{n}}, {% endfor %}.",
-            {'nums':[]},
-            "Empty: done."
-        )
+        self.try_render("Empty: {% for n in num %}{{n}}, {% endfor %}.",
+                        {'nums': []}, "Empty: done.")
 
     def test_multiline_loops(self):
         self.try_render(
             "Look: \n{% for n in nums %}\n{{n}}, \n{% endfor %}done.",
-            {'nums':[1, 2, 3]},
-            "Look: \n\n1, \n\2, \n\n3, \ndone."
-        )
+            {'nums': [1, 2, 3]}, "Look: \n\n1, \n\2, \n\n3, \ndone.")
 
     def test_multiple_loops(self):
-        self.try_render(
-            "{% for n in nums %}{{n}}{% endfor %} and "
-                                    "{% for n in nums %}{{n}}{% endfor %}",
-            {'nums': [1,2,3]},
-            "123 and 123"
-        )
-
-
+        self.try_render("{% for n in nums %}{{n}}{% endfor %} and "
+                        "{% for n in nums %}{{n}}{% endfor %}",
+                        {'nums': [1, 2, 3]}, "123 and 123")
